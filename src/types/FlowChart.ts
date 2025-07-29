55965e8dfd80d93178b751ca2f829ed1267145cd
liumengjie <liumengjie@shimo.im>
2025-07-29 10:06:53 +0800

    feat: update flowchart api

--- src/types/FlowChart.ts
+++ src/types/FlowChart.ts
@@ -9,4 +9,25 @@ export interface EventMap {
   }
 }
 
-export interface Editor extends BaseEditor<EventMap> {}
+export interface Editor extends BaseEditor<EventMap> {
+  /**
+   * 显示历史侧边栏
+   * @since co-1.4
+   */
+  showHistory: (options?: {}) => Promise<void>
+  /**
+   * 隐藏历史侧边栏
+   * @since co-1.4
+   */
+  hideHistory: (options?: {}) => Promise<void>
+  /**
+   * 显示版本侧边栏
+   * @since co-1.4
+   */
+  showRevision: (options?: {}) => Promise<void>
+  /**
+   * 隐藏版本侧边栏
+   * @since co-1.4
+   */
+  hideRevision: (options?: {}) => Promise<void>
+}
